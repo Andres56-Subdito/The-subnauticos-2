ca806965cc6c88e1e2412de7d6985f33691185c0
DyannStudent <dilan.hernandez@alumnos.ucentral.cl>
2026-09-25 10:32:14 -0300

    Lógica de modificación del DOM al hacer clic en el botón

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const botonMasInfo = document.querySelector('#btn-mas-info');
 const textoExtra = document.querySelector('#texto-extra');
 
 botonMasInfo.addEventListener('click', () => {
-	console.log("Clic detectado en mi sección");
+	if (textoExtra.hidden) {
+		textoExtra.hidden = false;
+		botonMasInfo.textContent = 'Leer menos';
+		botonMasInfo.setAttribute('aria-expanded', 'true');
+	} else {
+		textoExtra.hidden = true;
+		botonMasInfo.textContent = 'Leer más';
+		botonMasInfo.setAttribute('aria-expanded', 'false');
+	}
 });
