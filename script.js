366799a155b193399ba9ac88ed055cf7b0b781dd
DyannStudent <dilan.hernandez@alumnos.ucentral.cl>
2026-09-25 10:31:10 -0300

    Event listener de clic al botón

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const botonMasInfo = document.querySelector('#btn-mas-info');
 const textoExtra = document.querySelector('#texto-extra');
+
+botonMasInfo.addEventListener('click', () => {
+	console.log("Clic detectado en mi sección");
+});
